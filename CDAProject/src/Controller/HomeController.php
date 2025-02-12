327cb25b90e8875d39b9be89f7475cb8ef0c826a
Tev969 <freezuka14@gmail.com>
2025-02-12 15:32:21 +0100

    Ajout home page

--- CDAProject/src/Controller/HomeController.php
+++ CDAProject/src/Controller/HomeController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController{
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(ArticleRepository $articleRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => $articleRepository->findAll(),
         ]);
     }
 }
